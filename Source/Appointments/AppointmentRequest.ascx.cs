4eb7cb32e42d05c1ca59bf0e74c34cba603254d7
Brian Dukes <bdukes@engagesoftware.com>
2009-07-14 21:09:06 +0000

    Fixed syntax error
    git-tfs-work-item:1527 associate
    git-tfs-id: [http://tfs.etg-inc.net:8080/tfs/Engage%20TFS%202010]$/Engage Booking;C3503

--- Source/Appointments/AppointmentRequest.ascx.cs
+++ Source/Appointments/AppointmentRequest.ascx.cs
@@ -1,4 +1,4 @@
-// <copyright file="ResponseDetail.ascx.cs" company="Engage Software">
+// <copyright file="AppointmentRequest.ascx.cs" company="Engage Software">
 // Engage: Events - http://www.EngageSoftware.com
 // Copyright (c) 2004-2009
 // by Engage Software ( http://www.engagesoftware.com )
@@ -13,6 +13,7 @@ namespace Engage.Dnn.Booking.Appointments
 {
     using System;
     using System.Globalization;
+    using System.Web;
     using Booking;
     using DotNetNuke.Services.Exceptions;
 
@@ -29,9 +30,7 @@ namespace Engage.Dnn.Booking.Appointments
         {
             base.OnInit(e);
             this.Load += this.Page_Load;
-
-            //this.SaveAppointmentButton.Click += this.SaveAppointmentButton_Click;
-
+            ////this.SaveAppointmentButton.Click += this.SaveAppointmentButton_Click;
         }
 
         /// <summary>
@@ -47,9 +46,10 @@ namespace Engage.Dnn.Booking.Appointments
                 {
                     this.StartDateTimePicker.SelectedDate = this.GetDateFromQueryString("startTime");
                     this.EndDateTimePicker.SelectedDate = this.GetDateFromQueryString("endTime");
-                    // this.SetupControl();
-                    // this.BindData();
+                    //// this.SetupControl();
+                    //// this.BindData();
                 }
+
                 this.SuccessModuleMessage.Visible = false;
             }
             catch (Exception exc)
@@ -58,115 +58,120 @@ namespace Engage.Dnn.Booking.Appointments
             }
         }
 
+        /////// <summary>
+        /////// Handles the OnClick event of the SaveAppointmentButton control.
+        /////// </summary>
+        /////// <param name="sender">The source of the event.</param>
+        /////// <param name="e">The <see cref="System.EventArgs"/> instance containing the appointment data.</param>
+        ////private void SaveAppointmentButton_OnClick(object sender, EventArgs e)
+        ////{
+        ////    try
+        ////    {
+        ////        if (this.Page.IsValid)
+        ////        {
+        ////            this.Save();
+        ////            this.DisplayFinalSuccess();
+        ////        }
+        ////    }
+        ////    catch (Exception exc)
+        ////    {
+        ////        Exceptions.ProcessModuleLoadException(this, exc);
+        ////    }
+        ////}
 
-        /// <summary>
-        /// Handles the OnClick event of the SaveAppointmentButton control.
-        /// </summary>
-        /// <param name="sender">The source of the event.</param>
-        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the appointment data.</param>
-        private void SaveAppointmentButton_OnClick(object sender, EventArgs e)
-        {
-            try
-            {
-                if (this.Page.IsValid)
-                {
-                    this.Save();
-                    this.DisplayFinalSuccess();
-                }
-            }
-            catch (Exception exc)
-            {
-                Exceptions.ProcessModuleLoadException(this, exc);
-            }
-        }
+        /////// <summary>
+        /////// Displays the final success.
+        /////// </summary>
+        ////private void DisplayFinalSuccess()
+        ////{
+        ////    this.SuccessModuleMessage.Visible = true;
+        ////    ////this.AddNewEvent.Visible = false;
+        ////    ////this.FooterMultiview.SetActiveView(this.FinalFooterView);
+        ////}
 
-       /// <summary>
-        /// Displays the final success.
-        /// </summary>
-        private void DisplayFinalSuccess()
-        {
-            this.SuccessModuleMessage.Visible = true;
-            //this.AddNewEvent.Visible = false;
-            //this.FooterMultiview.SetActiveView(this.FinalFooterView);
-        }
+        /////// <summary>
+        /////// This method will either update or create an event based on the current context of EventId
+        /////// </summary>
+        ////private void Save()
+        ////{
+        ////    if (this.AppointmentId.HasValue)
+        ////    {
+        ////        this.Update();
+        ////    }
+        ////    else
+        ////    {
+        ////        this.Insert();
+        ////    }
+        ////}
 
-        /// <summary>
-        /// This method will either update or create an event based on the current context of EventId
-        /// </summary>
-        private void Save()
-        {
-            if (this.AppointmentId.HasValue)
-            {
-                this.Update();
-            }
-            else
-            {
-                this.Insert();
-            }
-        }
+        /////// <summary>
+        /////// This method is responsible for updating an event which already exists in the data store.
+        /////// </summary>
+        ////private void Update()
+        ////{
+        ////    ////int timeZoneOffsetMinutes;
+        ////    int? appointmentId = this.AppointmentId;
+        ////    ////if (appointmentId.HasValue && int.TryParse(this.TimeZoneDropDownList.SelectedValue, NumberStyles.Integer, CultureInfo.InvariantCulture, out timeZoneOffsetMinutes))
+        ////    ////{
+        ////    ////    TimeSpan timeZoneOffset = new TimeSpan(0, timeZoneOffsetMinutes, 0);
+        ////    ////    if (this.InDaylightTimeCheckBox.Checked)
+        ////    ////    {
+        ////    ////        timeZoneOffset = timeZoneOffset.Add(new TimeSpan(1, 0, 0));
+        ////    ////    }
 
-        /// <summary>
-        /// This method is responsible for updating an event which already exists in the data store.
-        /// </summary>
-        private void Update()
-        {
-            //int timeZoneOffsetMinutes;
-            int? appointmentId = this.AppointmentId;
-            //if (appointmentId.HasValue && int.TryParse(this.TimeZoneDropDownList.SelectedValue, NumberStyles.Integer, CultureInfo.InvariantCulture, out timeZoneOffsetMinutes))
-            //{
-            //    TimeSpan timeZoneOffset = new TimeSpan(0, timeZoneOffsetMinutes, 0);
-            //    if (this.InDaylightTimeCheckBox.Checked)
-            //    {
-            //        timeZoneOffset = timeZoneOffset.Add(new TimeSpan(1, 0, 0));
-            //    }
+        ////        Appointment appointment = Appointment.Load(appointmentId.Value);
+        ////        ////appointment.StartDateTime = this.StartDateTimePicker.SelectedDate.Value;
+        ////        ////appointment.EndDateTime = this.EndDateTimePicker.SelectedDate.Value;
+        ////        ////appointment.Title = this.EventTitleTextBox.Text;
+        ////        ////appointment.Description = this.EventDescriptionTextEditor.Text;
+        ////        ////...
+        ////        appointment.Save(this.UserId);
+        ////    ////}
+        ////}
 
-                Appointment appointment = Appointment.Load(appointmentId.Value);
-                //appointment.StartDateTime = this.StartDateTimePicker.SelectedDate.Value;
-                //appointment.EndDateTime = this.EndDateTimePicker.SelectedDate.Value;
-                //appointment.Title = this.EventTitleTextBox.Text;
-                //appointment.Description = this.EventDescriptionTextEditor.Text;
-                //...
-                appointment.Save(this.UserId);
-            //}
-        }
-
-        /// <summary>
-        /// Based on the values entered by the user for the event, this method will populate an event object and call the Event object's save method.
-        /// </summary>
-        private void Insert()
-        {
+        /////// <summary>
+        /////// Based on the values entered by the user for the event, this method will populate an event object and call the Event object's save method.
+        /////// </summary>
+        ////private void Insert()
+        ////{
             ////int timeZoneOffsetMinutes;
-            //if (int.TryParse(this.TimeZoneDropDownList.SelectedValue, NumberStyles.Integer, CultureInfo.InvariantCulture, out timeZoneOffsetMinutes))
-            //{
-                //TimeSpan timeZoneOffset = new TimeSpan(0, timeZoneOffsetMinutes, 0);
-                //if (this.InDaylightTimeCheckBox.Checked)
-                //{
-                //    timeZoneOffset = timeZoneOffset.Add(new TimeSpan(1, 0, 0));
-                //}
+            ////if (int.TryParse(this.TimeZoneDropDownList.SelectedValue, NumberStyles.Integer, CultureInfo.InvariantCulture, out timeZoneOffsetMinutes))
+            ////{
+            ////    TimeSpan timeZoneOffset = new TimeSpan(0, timeZoneOffsetMinutes, 0);
+            ////    if (this.InDaylightTimeCheckBox.Checked)
+            ////    {
+            ////        timeZoneOffset = timeZoneOffset.Add(new TimeSpan(1, 0, 0));
+            ////    }
 
-                //DateTime eventStart = this.StartDateTimePicker.SelectedDate.Value;
-                //DateTime eventEnd = this.EndDateTimePicker.SelectedDate.Value;
-                //Appointment appointment = Appointment.Create(
-                //        this.PortalId,
-                //        this.ModuleId,
-                //        this.UserInfo.Email,
-                //        this.EventTitleTextBox.Text,
-                //        this.EventOverviewTextEditor.Text,
-                //        this.EventDescriptionTextEditor.Text,
-                //        eventStart,
-                //        eventEnd,
-                //        timeZoneOffset,
-                //        this.EventLocationTextBox.Text,
-                //        this.FeaturedCheckBox.Checked,
-                //        this.AllowRegistrationsCheckBox.Checked,
-                //        //this.RecurrenceEditor.GetRecurrenceRule(eventStart, eventEnd),
-                //        this.LimitRegistrationsCheckBox.Checked && this.LimitRegistrationsCheckBox.Visible ? (int?)this.RegistrationLimitTextBox.Value : null,
-                //        this.InDaylightTimeCheckBox.Checked, String.Empty
-                //        );
+            ////    DateTime eventStart = this.StartDateTimePicker.SelectedDate.Value;
+            ////    DateTime eventEnd = this.EndDateTimePicker.SelectedDate.Value;
+            ////    Appointment appointment = Appointment.Create(
+            ////            this.PortalId,
+            ////            this.ModuleId,
+            ////            this.UserInfo.Email,
+            ////            this.EventTitleTextBox.Text,
+            ////            this.EventOverviewTextEditor.Text,
+            ////            this.EventDescriptionTextEditor.Text,
+            ////            eventStart,
+            ////            eventEnd,
+            ////            timeZoneOffset,
+            ////            this.EventLocationTextBox.Text,
+            ////            this.FeaturedCheckBox.Checked,
+            ////            this.AllowRegistrationsCheckBox.Checked,
+            ////            this.RecurrenceEditor.GetRecurrenceRule(eventStart, eventEnd),
+            ////            this.LimitRegistrationsCheckBox.Checked && this.LimitRegistrationsCheckBox.Visible ? (int?)this.RegistrationLimitTextBox.Value : null,
+            ////            this.InDaylightTimeCheckBox.Checked, String.Empty
+            ////            );
 
-                //appointment.Save(this.UserId);
-            //}
+            ////    appointment.Save(this.UserId);
+        ////}
 
+        /// <summary>
+        /// Gets a date from the <see cref="HttpRequest.QueryString"/> (handling specific formatting requirements).
+        /// </summary>
+        /// <param name="parameterName">Name of the <see cref="HttpRequest.QueryString"/> parameter with the date value.</param>
+        /// <returns>The date value of the given <paramref name="parameterName"/>, or <c>null</c> if there is not parameter with that name, or the parameter's
+        /// value is not in the correct format.</returns>
         private DateTime? GetDateFromQueryString(string parameterName)
         {
             DateTime dateValue;
