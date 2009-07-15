bfc7269fc35e2f4ce9cee322bcdbb8d9925cacf4
Brian Dukes <bdukes@engagesoftware.com>
2009-07-15 19:23:20 +0000

    Fixed build break (from half-checked-in code)
    git-tfs-work-item:1530 associate
    git-tfs-id: [http://tfs.etg-inc.net:8080/tfs/Engage%20TFS%202010]$/Engage Booking;C3512

---
+++ Source/Display/Approval.ascx
@@ -0,0 +1,2 @@
+<%@ Control Language="c#" AutoEventWireup="false" Inherits="Engage.Dnn.Booking.Display.Approval" CodeBehind="Approval.ascx.cs" %>
+<asp:GridView ID="AppointmentsGrid" runat="server" />
---
+++ Source/Display/Approval.ascx.cs
@@ -0,0 +1,60 @@
+// <copyright file="Approval.ascx.cs" company="Engage Software">
+// Engage: Booking - http://www.EngageSoftware.com
+// Copyright (c) 2004-2009
+// by Engage Software ( http://www.engagesoftware.com )
+// </copyright>
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+// CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+// DEALINGS IN THE SOFTWARE.
+
+namespace Engage.Dnn.Booking.Display
+{
+    using System;
+    using Booking;
+    using DotNetNuke.Services.Exceptions;
+
+    /// <summary>
+    /// Control to display the events calendar view
+    /// </summary>
+    public partial class Approval : ModuleBase
+    {
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Init"/> event.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.EventArgs"/> object that contains the event data.</param>
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+
+            this.Load += this.Page_Load;
+        }
+
+        /// <summary>
+        /// Handles the Load event of the Page control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        private void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                this.BindData();
+            }
+            catch (Exception exc)
+            {
+                Exceptions.ProcessModuleLoadException(this, exc);
+            }
+        }
+
+        /// <summary>
+        /// Binds the data.
+        /// </summary>
+        private void BindData()
+        {
+            this.AppointmentsGrid.DataSource = AppointmentCollection.Load(this.PortalId, ListingMode.All, true, this.IsFeatured);
+            this.AppointmentsGrid.DataBind();
+        }
+    }
+}
