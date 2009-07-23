76fb861bc7576802400faab87c91f47462529a9b
Brad Montgomery <bmontgomery@engagesoftware.com>
2009-07-23 22:18:11 +0000

    Adding CsvWriter, grr.
    git-tfs-force:Override
    git-tfs-id: [http://tfs.etg-inc.net:8080/tfs/Engage%20TFS%202010]$/Engage Booking;C3684

---
+++ Source/Components/CsvWriter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.IO;
+using System.Data;
+
+namespace Engage.Dnn.Booking.Components
+{
+    public class CsvWriter
+    {
+        public static string WriteToString(DataTable table, bool header, bool quoteall)
+        {
+            StringWriter writer = new StringWriter();
+            WriteToStream(writer, table, header, quoteall);
+            return writer.ToString();
+        }
+
+        public static void WriteToStream(TextWriter stream, DataTable table, bool header, bool quoteall)
+        {
+            if (header)
+            {
+                for (int i = 0; i < table.Columns.Count; i++)
+                {
+                    WriteItem(stream, table.Columns[i].Caption, quoteall);
+                    if (i < table.Columns.Count - 1)
+                        stream.Write(',');
+                    else
+                        stream.Write('\n');
+                }
+            }
+            foreach (DataRow row in table.Rows)
+            {
+                for (int i = 0; i < table.Columns.Count; i++)
+                {
+                    WriteItem(stream, row[i], quoteall);
+                    if (i < table.Columns.Count - 1)
+                        stream.Write(',');
+                    else
+                        stream.Write('\n');
+                }
+            }
+        }
+
+        private static void WriteItem(TextWriter stream, object item, bool quoteall)
+        {
+            if (item == null)
+                return;
+            string s = item.ToString();
+            if (quoteall || s.IndexOfAny("\",\x0A\x0D".ToCharArray()) > -1)
+                stream.Write("\"" + s.Replace("\"", "\"\"") + "\"");
+            else
+                stream.Write(s);
+        }
+    }
+}
