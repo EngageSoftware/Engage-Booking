b0dd36a627a3c6200375dc8e3bd89455a5b2cf1e
Brad Montgomery <bmontgomery@engagesoftware.com>
2009-07-23 22:20:19 +0000

    git-tfs-force:Override git-tfs-id: [http://tfs.etg-inc.net:8080/tfs/Engage%20TFS%202010]$/Engage Booking;C3685

--- Source/ExportData.ascx.cs
+++ Source/ExportData.ascx.cs
@@ -10,11 +10,11 @@ namespace Engage.Dnn.Booking
 {
     public partial class ExportData : ModuleBase
     {
-        static string FileName
+        static string BaseFileName
         {
             get
             {
-                return "AppointmentsData-" + DateTime.Now.Year + DateTime.Now.Month + DateTime.Now.Day + ".csv";
+                return "AppointmentsData-" + DateTime.Now.Year + DateTime.Now.Month + DateTime.Now.Day;
             }
         }
 
@@ -36,7 +36,7 @@ namespace Engage.Dnn.Booking
             //dt.Load(AppointmentSqlDataProvider.GetAppointments(this.ModuleId, 'AppointmentId', null, null));
             
             ////string outputString = CsvWriter.WriteToString(dt, false, false);
-            //var outputFile = new FileStream(this. + "AppointmentsData-" + DateTime.Now.Year + DateTime.Now.Month + DateTime.Now.Day + ".csv", FileAccess.Write);
+            //var outputFile = new FileStream(this.ModulePath + BaseFileName + ".csv", FileAccess.Write);
             //CsvWriter.WriteToStream(outputFile, dt, false, false);
 
             //Server.Transfer(FileName).
